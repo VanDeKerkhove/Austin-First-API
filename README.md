43c4b67725db40521989fc54abe6d2e970f7dab7
austinv <austin.f.vandekerkhove@gmail.com>
2021-09-12 23:52:41 -0400

    added express web server at port 3000

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ First API
 
 - Tested git origin, remote, branches and VS code integration with github.
 - Learned about "git pull --allow-unrelated-histories"
+
+Running:
+
+- type `node .` or `node index.js` to launch the web server on port 3000.
+- View at `http://localhost:3000/`
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log('hello world ▓╚∟');
-console.log(process.platform);
-console.log(process.env);
+// console.log('hello world ▓╚∟');
+// console.log(process.platform);
+// console.log(process.env);
+
+const express = require("express");
+const app = express();
+
+app.get('/', (req,resp) =>{
+    resp.send("Welcome to the jungle.");
+    // resp.send('hello world ▓╚∟');
+});
+
+app.listen(3000, () => {
+    console.log("App available on port 3000");
+});
